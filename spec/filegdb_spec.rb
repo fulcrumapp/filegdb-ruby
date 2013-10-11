a5013ff500a7a4650b7e69feb3f82a10a5ad9372
Zac McCormick <zac.mccormick@gmail.com>
2013-10-11 14:51:47 -0400

    Add specs for setting up rows

--- spec/filegdb_spec.rb
+++ spec/filegdb_spec.rb
@@ -94,5 +94,41 @@ describe "FileGDB" do
       row.set_string('string_field', 'a string value')
       lambda { row.get_string('string_field_that_doesnt_exist') }.should raise_error
     end
+
+    it 'creates a point shape buffer using #new' do
+      FileGDB::PointShapeBuffer.new.should_not be_nil
+    end
+
+    it 'can setup a geometry' do
+      shape = FileGDB::PointShapeBuffer.new
+      lambda { shape.setup(1) }.should_not raise_error
+    end
+
+    it 'throws an exception when setting up a point shape incorrectly' do
+      shape = FileGDB::PointShapeBuffer.new
+      lambda { shape.setup(2) }.should raise_error
+    end
+
+    it 'fetches the point object after setting it up' do
+      shape = FileGDB::PointShapeBuffer.new
+      shape.setup(1)
+      shape.get_point.should be_instance_of(FileGDB::Point)
+    end
+
+    it 'sets the geometry of a point' do
+      row = @table.create_row_object
+      shape = FileGDB::PointShapeBuffer.new
+      shape.setup(1)
+      point = shape.get_point
+      point.x = -82.23233;
+      point.y = 27.457347347;
+      lambda { row.set_geometry(shape) }.should_not raise_error
+    end
+
+    it 'throws an exception when setting up the geometry incorrectly' do
+      row = @table.create_row_object
+      lambda { row.set_geometry(nil) }.should raise_error
+    end
+
   end
 end
