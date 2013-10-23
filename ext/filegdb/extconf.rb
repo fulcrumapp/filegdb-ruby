9e43d13650749418c4eb6f19397462a33e827e0b
Zac McCormick <zac.mccormick@gmail.com>
2013-10-23 17:18:48 -0400

    Force compilation with GCC on OSX Mavericks

--- ext/filegdb/extconf.rb
+++ ext/filegdb/extconf.rb
@@ -10,4 +10,48 @@ $libs = append_library $libs, 'FileGDBAPI'
 
 $LDFLAGS << " -Wl,-rpath,#{File.join(filegdb_path, 'lib')}"
 
+if RUBY_PLATFORM =~ /darwin12/
+  # libFileGDBAPI requires GCC compilation because it's C++ symbols are incompatible with LLVM 5.0
+  #
+  # To install GCC on OSX:
+  #
+  #   $ brew tap homebrew/versions
+  #   $ brew install --enable-cxx gcc49
+  #
+
+  msg = <<-GCC
+
+
+\e[31m
+===========================================================================================================================
+===========================================================================================================================
+
+The filegdb gem requires GCC on OSX Mavericks because the C++ symbols in libFileGDBAPI.dylib are incompatible with LLVM 5.0
+
+To install GCC on OSX:
+
+   $ brew tap homebrew/versions
+   $ brew install --enable-cxx gcc49
+
+Note: Installing GCC through homebrew is safe and will not interfere with your Xcode installation.
+
+===========================================================================================================================
+===========================================================================================================================
+\e[0m
+
+GCC
+
+  if `which gcc-4.00000`.strip.length == 0
+    puts msg
+    exit
+  end
+
+  require 'rbconfig'
+  RbConfig::MAKEFILE_CONFIG['CC']  = 'gcc-4.9'
+  RbConfig::MAKEFILE_CONFIG['CXX'] = 'gcc-4.9'
+
+  # remove -Wshorten-64-to-32 since it's incompatible with GCC
+  $warnflags = $warnflags.gsub(/-Wshorten-64-to-32/, '')
+end
+
 create_makefile 'filegdb/filegdb'
